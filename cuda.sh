3f515737a81da6d5be5db5f4c2810b5ff1aa706b
Asim Siddiqui <riksha_driver008@hotmail.com>
2021-01-03 14:06:41 +1100

    Create cuda.sh

---
+++ cuda.sh
@@ -0,0 +1,5 @@
+# PATH for CUDA
+if [ -d "/usr/local/cuda/bin/" ]; then
+    export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
+    export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+fi
